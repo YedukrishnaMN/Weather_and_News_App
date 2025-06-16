60133098f7d176acd4f5110a68cd562f81ff9f26
YedukrishnaMN <yemn22cs@cmrit.ac.in>
2025-06-16 22:07:12 +0530

    Create theme_notifier.dart

---
+++ lib/core/theme/theme_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.black),
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black87,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.white),
+    ),
+  );
+}
